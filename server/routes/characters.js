e96a26652c561c2b6fd7bfb5d57a64aa5f1e4f9c
Josue Vicente <josue.vicente@ufl.edu>
2025-09-23 00:24:20 -0400

    added character.js to server and index to client
    added character.js to server to be able to expose the individual character.html elemet from the client
    
    added the index.html with is the entry point for the cliet

--- server/routes/characters.js
+++ server/routes/characters.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import giftData from '../data/gifts.js'
+import giftData from '../data/characters.js'
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirName = path.dirname(__fileName);
@@ -12,8 +12,8 @@ router.get('/', (req, res) => {
     res.status(200).json(giftData);
 })
 
-router.get ('/:giftId', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirName, '../public/gift.html'));
+router.get ('/:characterId', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirName, '../public/character.html'));
 })
 
 export default router;
