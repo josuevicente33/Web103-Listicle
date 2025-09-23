cf14f7366066e2e4126fade26aa1857a26f9f1ae
Josue Vicente <josue.vicente@ufl.edu>
2025-09-23 00:02:31 -0400

    Update server.js
    finished the server file

--- server/routes/server.js
+++ server/routes/server.js
@@ -6,4 +6,14 @@ const app = express();
 app.use('/public', express.static('./public'))
 app.use('/scripts', express.static('./scripts'))
 
-app.use('/gifts', )
+app.use('/characters', characterRouter);
+
+app.get('/', (req, res) => {
+    res.status(200).send('<h1 style="text-align: center; margin-top: 50px;"> Marverl Rivals Characters </h1>')
+})
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+})
