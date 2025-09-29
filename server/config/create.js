c87dadea23d369b7e4edc430c513d246e38edcc9
Josue Vicente <70499804+josuevicente33@users.noreply.github.com>
2025-09-28 22:58:33 -0400

    create and database update
    updated the create.js to create a table when we run it, basically itll be our inital api when launching the app for the first time
    
    updated the database.js to start the connection to our database
    
    the routes are our actual api

--- server/config/create.js
+++ server/config/create.js
@@ -3,3 +3,64 @@ import pool from './database.js';
 import dotenv from './dotenv.js';
 
 import characterData from '../data/characters.json'
+
+const createCharacterTable = async () => {
+    const createTableQuery = `
+    DROP TABLE IF EXISTS characters;
+
+    CREATE TABLE IF NOT EXISTS characters (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        slug VARCHAR(100) UNIQUE NOT NULL,
+        alias VARCHAR(100),
+        role VARCHAR(50),
+        affiliations TEXT[],
+        image TEXT,
+        description TEXT,
+        infoUrl TEXT
+    )
+    `
+    try {
+        const res = await pool.query(createTableQuery);
+        console.log("Character table created:", res);
+    } catch (error) {
+        console.error("Error creating character table:", error);
+    }
+};
+
+const insertCharacterData = async () => {
+    await createCharacterTable();
+
+    characterData.forEach((character) => {
+        const insertQuery = `
+        INSERT INTO characters (id, name, slug, alias, role, affiliations, image, description, infoUrl)
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)
+        ON CONFLICT (id) DO NOTHING;
+        `;
+        const values = [
+            character.id,
+            character.name,
+            character.slug,
+            character.alias,
+            character.role,
+            character.affiliations,
+            character.image,
+            character.description,
+            character.infoUrl
+        ];
+
+        try {
+            pool.query(insertQuery, values, (err, res) => {
+                if (err) {
+                    console.error("Error inserting character:", err);
+                    return;
+                }
+                console.log(`Character with ID ${character.id} inserted or already exists.`);
+            });
+        } catch (error) {
+            console.error("Error inserting character:", error);
+        }
+    });
+};
+
+insertCharacterData();
